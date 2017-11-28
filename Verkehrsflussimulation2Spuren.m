210abca9799337ba2938f41dfe0b4be47362e6c7
Philipp <seitzph@ymail.com>
2017-11-28 08:12:53 +0100

    test merge

--- Verkehrsflussimulation2Spuren.m
+++ Verkehrsflussimulation2Spuren.m
@@ -10,7 +10,7 @@ rho=[0.16 0.2 0.25]; %Mittlere Dichte
 tp=[0 0.2 0.5 0.8]; %Trödelwahrscheinlichkeit
 troedelwsnlkt=tp(2);
 
-lanes=5; %Anzahl an Spuren
+lanes=2; %Anzahl an Spuren
 strasse=cell(lanes,zellen); %Strecke
 
 %Anzahl der Fahrzeuge
@@ -36,14 +36,14 @@ dtmax=30;
 PlotHighway(strasse);
 
 %%% Check wie viele Autos sich auf der Strasse befinden
-% summe=0;
-% for i=1:lanes
-%     for j=1:zellen
-%         if~isempty(strasse{i,j})
-%             summe=summe+1;
-%         end
-%     end
-% end
+summe=0;
+for i=1:lanes
+    for j=1:zellen
+        if~isempty(strasse{i,j})
+            summe=summe+1;
+        end
+    end
+end
 
 idxmod = @(x, indexRange) mod(x - 1, indexRange) + 1;
 
@@ -69,34 +69,25 @@ while dt>-dtmax
                 % Nach links wechseln
                 tempLane=lane;
                 
+                % Nach rechts wechseln
+%                 tempLane=lane;
+                %Nach rechts wechseln, wenn genau rechts neben Auto frei
+                if lane<lanes &&...
+                        CheckLane(lane+1,zelle,strasse,0,0)>-1 %rechte Spur ist frei
+                    tempLane=lane+1;
+                    vehicle.gewechselt=true;
+                end     
+                
                 %Auf aktueller Spur muss gebremst werden
-                if CheckLane(lane,zelle,strasse,1,vehicle.v)<vehicle.v
+                if ~vehicle.gewechselt &&...
+                        CheckLane(lane,zelle,strasse,1,vehicle.v)<vehicle.v
                     %Nach links wechslen, wenn genau links neben Auto frei
-                    if lane>1 && CheckLane(lane-1,zelle,strasse,0,0)>-1
+                    if lane>1 && CheckLane(lane-1,zelle,neueStrasse,0,0)>-1
                         tempLane=lane-1;
                         vehicle.gewechselt=true;
                     end
                 end
-                neueStrasse{tempLane,zelle} = vehicle;
-            end
-        end
-    end
-    
-    strasse = neueStrasse;
-    neueStrasse = cell(lanes,zellen);
-    
-    for lane=1:lanes
-        for zelle = 1:zellen
-            
-            vehicle = strasse{lane,zelle};
-            if  ~isempty(vehicle)
                 
-                % Nach rechts wechseln
-                tempLane=lane;
-                %Nach rechts wechseln, wenn genau rechts neben Auto frei
-                if ~vehicle.gewechselt && lane<lanes && CheckLane(lane+1,zelle,strasse,0,0)>-1 %rechte Spur ist frei
-                    tempLane=lane+1;
-                end                
                 neueStrasse{tempLane,zelle} = vehicle;
             end
         end
@@ -119,7 +110,7 @@ while dt>-dtmax
                 vehicle.v=CheckLane(lane,zelle,strasse,1,vehicle.v);
                 
                 % Trödeln
-                vehicle.v = vehicle.v - (vehicle.v ~= 0 && rand()+troedelwsnlkt > 1);
+%                 vehicle.v = vehicle.v - (vehicle.v ~= 0 && rand()+troedelwsnlkt > 1);
                 
                 % Bewegen
                 neueStrasse{lane,idxmod(zelle+vehicle.v,zellen)} = vehicle;
@@ -131,14 +122,14 @@ while dt>-dtmax
     PlotHighway(strasse);
     
     %%% Check wie viele Autos sich auf der Strasse befinden
-%     summe=0;
-%     for i=1:lanes
-%         for j=1:zellen
-%             if~isempty(strasse{i,j})
-%                 summe=summe+1;
-%             end
-%         end
-%     end
+    summe=0;
+    for i=1:lanes
+        for j=1:zellen
+            if~isempty(strasse{i,j})
+                summe=summe+1;
+            end
+        end
+    end
     
     
     pause(0.51);
